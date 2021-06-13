44b421c10eecece83b7cc6d524c0977c39f06fa0
Grace <frankgracey@gmail.com>
2021-06-13 11:31:04 +0100

    added validation for next of Kin and Bank Details

---
+++ src/validations/BankDetailsValidation.js
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export const bankDetailsSchema = Joi.object({
+  account_name: Joi.string().required().min(1).max(255).trim(),
+  bank_name: Joi.string().required().min(1).max(255).trim(),
+  account_number: Joi.string()
+    .pattern(/^[0-9]+$/, { name: "numbers" })
+    .required()
+    .min(11)
+    .max(11),
+});
