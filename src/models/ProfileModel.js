75456a85ba560b121dad8cb9f4d6f071440f24c5
Grace <frankgracey@gmail.com>
2021-06-13 06:53:03 +0100

    added address to profile schema

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -1,11 +1,43 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  street_address: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 255,
+  },
+  street_address2: {
+    type: String,
+    trim: true,
+    maxlength: 255,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 255,
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 255,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 255,
+  },
+});
 
 const profileSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      enum: [ 'Mr', 'Mrs', 'Miss', 'Mr' ],
-      default: 'Mr',
+      enum: ["Mr", "Mrs", "Miss", "Mr"],
+      default: "Mr",
     },
     birthday: {
       type: Date,
@@ -29,7 +61,7 @@ const profileSchema = new mongoose.Schema(
     id_card_url: { type: String, minlength: 7, maxlength: 255 },
     id_type: {
       type: String,
-      enum: [ 'voters_card', 'drivers_licence', 'NIN', 'passport' ],
+      enum: ["voters_card", "drivers_licence", "NIN", "passport"],
       required: true,
     },
 
@@ -39,10 +71,20 @@ const profileSchema = new mongoose.Schema(
       minlength: 11,
       maxlength: 11,
     },
+    address: {
+      type: addressSchema,
+      required: true,
+    },
 
     isDeleted: { type: Boolean, default: false },
+
+    role: {
+      type: mongoose.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.model('profiles', profileSchema);
+export default mongoose.model("profiles", profileSchema);
