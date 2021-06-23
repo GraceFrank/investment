be3e173aba87f110101fb4782418669456ad3a2e
Grace <frankgracey@gmail.com>
2021-06-23 21:01:54 +0100

    modified the mailler endpoints and added more

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,21 +1,23 @@
 import dotenv from 'dotenv';
 import Mailgun from 'mailgun-js';
-import generateConfirmationEmail from '../constants/mail/activation_email';
-import generateEmailConfirmedMail from '../constants/mail/email-confirmed';
 
 dotenv.config();
 
 const API_KEY = process.env.SMTP_API_KEY;
 const DOMAIN = process.env.SMTP_DOMAIN_NAME;
 const mailer = Mailgun({ apiKey: API_KEY, domain: DOMAIN });
+const { UI_BASE_URL } = process.env;
 
-export const sendActivationMail = ({ name, email, confirmationUrl }) => {
+export const sendActivationMail = ({ name, email, token }) => {
   const data = {
     from: 'Abudanza <support@mg.abudanza.africa>',
     to: email,
-    subject: 'Confirm Your Email',
-    text: 'Confirm your email',
-    html: generateConfirmationEmail(name, confirmationUrl),
+    subject: 'Abudanza: Confirm Your Email',
+    template: 'activate_account_request',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      name,
+      confirmation_url: `${UI_BASE_URL}/verification/?confirmation_token=${token}`,
+    }),
   };
 
   return mailer.messages().send(data, (error, body) => {
@@ -25,13 +27,31 @@ export const sendActivationMail = ({ name, email, confirmationUrl }) => {
 };
 
 export const sendEmailConfirmedMail = ({ name, email }) => {
-  const UiLoginUrl = `${process.env.UI_BASE_URL}/login/`;
   const data = {
     from: 'Abudanza <support@mg.abudanza.africa>',
     to: email,
     subject: 'Abudanza Account Activated',
-    text: `Congratulation ${name}, your account has been`,
-    html: generateEmailConfirmedMail(name, UiLoginUrl),
+    template: 'email_confirmed',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      firstName: name,
+      loginUrl: `${UI_BASE_URL}/login/`,
+    }),
+  };
+  return mailer.messages().send(data, (error, body) => {
+    if (error) return Promise.reject(error);
+    return Promise.resolve(body);
+  });
+};
+
+export const sendPasswordResetEmail = ({ email, token }) => {
+  const data = {
+    from: 'Abudanza <support@mg.abudanza.africa>',
+    to: email,
+    subject: 'Abudanza Password Reset',
+    template: 'password_reset',
+    'h:X-Mailgun-Variables': JSON.stringify({
+      password_reset_url: `${UI_BASE_URL}/reset-password/?reset_token=${token}`,
+    }),
   };
 
   return mailer.messages().send(data, (error, body) => {
