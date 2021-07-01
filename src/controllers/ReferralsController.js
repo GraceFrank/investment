9d4f4b3927e6016a1283a47794787464697f4da2
Grace <frankgracey@gmail.com>
2021-07-01 10:27:52 +0100

    removed fake data from referrals

--- src/controllers/ReferralsController.js
+++ src/controllers/ReferralsController.js
@@ -2,34 +2,18 @@
 import UserModel from '../models/UserModel';
 import ReferralModel from '../models/ReferralModel';
 
-const fakeData = [
-  {
-    account_id: 'YPoid',
-    bonus_earned: 1000,
-    invested: true,
-    registration_date: '2021-06-13T06:37:51.250+00:00',
-  },
-
-  {
-    account_id: 'yTu!odA',
-    bonus_earned: 0,
-    invested: false,
-    registration_date: '2021-06-13T06:37:51.250+00:00',
-  },
-  {
-    account_id: '1BuoRd',
-    bonus_earned: 0,
-    invested: false,
-    registration_date: '2021-06-13T06:37:51.250+00:00',
-  },
-];
-
 export const getReferrals = async (req, res, next) => {
   try {
+    const referrals = ReferralModel.find({ referrerId: req.user.account_id });
+    const totalEarned = referrals.reduce(
+      (total, referral) => total + referral.bonus,
+      0
+    );
+
     return res.status(200).send({
       statusCode: 200,
       status: 'success',
-      payload: { totalEarned: 1000, referrals: fakeData },
+      payload: { totalEarned, referrals },
     });
   } catch (err) {
     return next(err, req, res, next);
